946bbe2a48daef5222b627455abb034f5dd8a766
RyanCohen70 <ryan@planetcohen.com>
2021-05-26 12:17:58 -0700

    clean-up

--- components/App.tsx
+++ components/App.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
-import {Header} from './Header';
-import {Nav, TPrimaryNavEntry, TSecondaryNavEntry} from './Nav';
-import {Main} from './Main';
+import { Header } from './Header';
+import { Nav, TPrimaryNavEntry } from './Nav';
+import { Main } from './Main';
 
 const primaryNavEntries: TPrimaryNavEntry[] = [
   { id: 'class', label: 'Class', isSelected: true },
   { id: 'year', label: 'Year', isSelected: false },
   { id: 'venue', label: 'Venue', isSelected: false },
   { id: 'genre', label: 'Genre', isSelected: false },
-]
+];
 
-const secondaryNavEntries = { 
-  'class': [
+const secondaryNavEntries = {
+  class: [
     { id: 'electronica', label: 'Electronica', isSelected: true },
     { id: 'ensemble-i', label: 'Ensemble I', isSelected: false },
     { id: 'ensemble-ii', label: 'Ensemble II', isSelected: false },
@@ -19,43 +19,31 @@ const secondaryNavEntries = {
     { id: 'scoring-the-game', label: 'Scoring the Game', isSelected: false },
     { id: 'viva-voce', label: 'Viva Voce', isSelected: false },
   ],
-  'year': [
+  year: [
     { id: '2019-20', label: '2019-20', isSelected: true },
     { id: '2018-19', label: '2018-19', isSelected: false },
     { id: '2017-18', label: '2017-18', isSelected: false },
   ],
-  'venue': [
+  venue: [
     { id: 'venue-43', label: 'Venue 43 (fall 2019)', isSelected: true },
     { id: 'venue-42', label: 'Venue 42 (spring 2019)', isSelected: false },
     { id: 'venue-41', label: 'Venue 41 (fall 2018)', isSelected: false },
   ],
-  'genre': [
+  genre: [
     { id: 'rock', label: 'Rock', isSelected: true },
     { id: 'country', label: 'Country', isSelected: false },
     { id: 'jazz', label: 'Jazz', isSelected: false },
     { id: 'pop', label: 'Pop', isSelected: false },
   ],
-}
+};
 
-export function App(){
-  console.log('turkeyman has returned')
-  const [primaryNavId, setPrimaryNavId] = React.useState(primaryNavEntries[0].id);
-  const handleClickPrimary = (id: string) => {
-    console.log('handleClickPrimary =>', id);
-    setPrimaryNavId(id);
-  }
-  return(
+export function App() {
+  return (
     <div>
-    <Header/>
-    <div className="container">
-      <Nav
-        primaryNavEntries={primaryNavEntries}
-        secondaryNavEntries={secondaryNavEntries[primaryNavId]}
-        primarySelectedId={primaryNavId}
-        onClickPrimary={handleClickPrimary}
-      />
-      <Main/>
+      <Header />
+      <div className='container'>
+        <Main />
+      </div>
     </div>
-  </div>
-  )
+  );
 }
